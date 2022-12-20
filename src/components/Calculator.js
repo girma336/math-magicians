08875a970e3b3ae50a5eff439480520931bef3f1
girma <girmatarekegn.gi@gmail.com>
2022-12-20 15:14:01 +0300

    Add full website component of math magicians

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -14,34 +14,39 @@ const Calculator = () => {
     setRsualts((obj) => calculate(obj, e.target.name));
   };
   return (
-    <div className="container">
-      <div className="screen-window">
-        <span>
-          {total}
-          {operation}
-          {next}
-        </span>
+    <div className="main-container">
+      <div className="text-container">
+        <h1>Let`s Do Some Math!</h1>
       </div>
-      <div className="keypad">
-        <button type="button" name="AC" onClick={handlClick}>AC</button>
-        <button type="button" name="+/-" onClick={handlClick}>+/-</button>
-        <button type="button" name="%" onClick={handlClick}>%</button>
-        <button type="button" name="÷" onClick={handlClick} className="color-i">÷</button>
-        <button type="button" name="7" onClick={handlClick}>7</button>
-        <button type="button" name="8" onClick={handlClick}>8</button>
-        <button type="button" name="9" onClick={handlClick}>9</button>
-        <button type="button" name="x" onClick={handlClick} className="color-i">x</button>
-        <button type="button" name="4" onClick={handlClick}>4</button>
-        <button type="button" name="5" onClick={handlClick}>5</button>
-        <button type="button" name="6" onClick={handlClick}>6</button>
-        <button type="button" name="-" onClick={handlClick} className="color-i">-</button>
-        <button type="button" name="1" onClick={handlClick}>1</button>
-        <button type="button" name="2" onClick={handlClick}>2</button>
-        <button type="button" name="3" onClick={handlClick}>3</button>
-        <button type="button" name="+" onClick={handlClick} className="color-i">+</button>
-        <button type="button" name="0" onClick={handlClick} className="zero">0</button>
-        <button type="button" name="." onClick={handlClick}>.</button>
-        <button type="button" name="=" onClick={handlClick} className="color-i">=</button>
+      <div className="container">
+        <div className="screen-window">
+          <span>
+            {total}
+            {operation}
+            {next}
+          </span>
+        </div>
+        <div className="keypad">
+          <button type="button" name="AC" onClick={handlClick}>AC</button>
+          <button type="button" name="+/-" onClick={handlClick}>+/-</button>
+          <button type="button" name="%" onClick={handlClick}>%</button>
+          <button type="button" name="÷" onClick={handlClick} className="color-i">÷</button>
+          <button type="button" name="7" onClick={handlClick}>7</button>
+          <button type="button" name="8" onClick={handlClick}>8</button>
+          <button type="button" name="9" onClick={handlClick}>9</button>
+          <button type="button" name="x" onClick={handlClick} className="color-i">x</button>
+          <button type="button" name="4" onClick={handlClick}>4</button>
+          <button type="button" name="5" onClick={handlClick}>5</button>
+          <button type="button" name="6" onClick={handlClick}>6</button>
+          <button type="button" name="-" onClick={handlClick} className="color-i">-</button>
+          <button type="button" name="1" onClick={handlClick}>1</button>
+          <button type="button" name="2" onClick={handlClick}>2</button>
+          <button type="button" name="3" onClick={handlClick}>3</button>
+          <button type="button" name="+" onClick={handlClick} className="color-i">+</button>
+          <button type="button" name="0" onClick={handlClick} className="zero">0</button>
+          <button type="button" name="." onClick={handlClick}>.</button>
+          <button type="button" name="=" onClick={handlClick} className="color-i">=</button>
+        </div>
       </div>
     </div>
   );
