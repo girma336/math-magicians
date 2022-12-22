6013c15be57007ac15ee63646b72ac75e0e9a684
BryteLitty <littyofficials@gmail.com>
2022-12-22 09:32:39 +0000

    add test for the calculate logic

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -1,1 +1,15 @@
 import calculate from '../logic/calculate';
+
+describe("Test calulate logic", () => {
+    const object = {
+        total: 8,
+        next: 4,
+        operation: "+"
+    }
+    it("calculate to check the AC", () => {
+        const temp = calculate(object, 'AC')
+        expect(temp.total).toBe(null)
+        expect(temp.next).toBe(null)
+        expect(temp.operation).toBe(null)
+    })
+});
